e638115a5884d7e645d17f12441a17313b8c6e5a
safacakir <cakirsafak@hotmail.com>
2023-03-02 13:42:09 +0300

    HttpMethodType eklendi.

--- amorphie.core/Base/Translation.cs
+++ amorphie.core/Base/Translation.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace amorphie.core.Base
 {
-    public abstract class Translation : EntityBase
+    public sealed class Translation : EntityBase
     {
         public string Language { get; set; } = string.Empty;
         public string Label { get; set; } = string.Empty;
---
+++ amorphie.core/Enums/HttpMethodType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amorphie.core.Enums
+{
+    public enum HttpMethodType : byte 
+    { 
+        CONNECT, 
+        DELETE, 
+        GET, 
+        HEAD, 
+        OPTIONS, 
+        POST, 
+        PUT, 
+        TRACE 
+    }
+}
