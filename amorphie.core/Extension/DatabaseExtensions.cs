294af13c297b6f22939138ed2d2474e65923b440
tsimsekburgan <TSimsek@burgan.com.tr>
2023-12-08 16:28:04 +0300

    Non Required

--- amorphie.core/Extension/DatabaseExtensions.cs
+++ amorphie.core/Extension/DatabaseExtensions.cs
@@ -15,12 +15,12 @@ public enum SortDirectionEnum
 public static class DatabaseExtensions
 {
 
-    public async static Task<IQueryable<TModel>> Sort<TModel>(this IQueryable<TModel> query, string SortColumn, SortDirectionEnum sortDirectionEnum)
+    public async static Task<IQueryable<TModel>> Sort<TModel>(this IQueryable<TModel> query, string SortColumn, SortDirectionEnum? sortDirectionEnum)
     {
         if (!string.IsNullOrEmpty(SortColumn))
         {
             string sortDirection = string.Empty;
-if(sortDirectionEnum == SortDirectionEnum.Asc)
+if(sortDirectionEnum.GetValueOrDefault(SortDirectionEnum.Asc) == SortDirectionEnum.Asc)
 {
                 sortDirection = "OrderBy";
 }
