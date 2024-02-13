42f0f2d7c25fe9c636212bb9d8a4111c7fc5aeca
ikarakayali <ibrahimkarakayali88@gmail.com>
2024-02-13 18:42:40 +0300

    Overload for AddVaultSecrets method

--- amorphie.core/Extension/VaultConfigExtension.cs
+++ amorphie.core/Extension/VaultConfigExtension.cs
@@ -1,37 +1,57 @@
-﻿using System;
-using Dapr.Client;
+﻿using Dapr.Client;
 
-namespace amorphie.core.Extension
+namespace amorphie.core.Extension;
+
+public static class VaultConfigExtension
 {
-
-    public static class VaultConfigExtension
+    public static async Task AddVaultSecrets(this IConfigurationBuilder builder, string? secretStoreName, string[] secretPaths)
     {
-        public static async Task AddVaultSecrets(this IConfigurationBuilder builder, string? secretStoreName, string[] secretPaths)
+        if (string.IsNullOrWhiteSpace(secretStoreName))
         {
-            if (string.IsNullOrWhiteSpace(secretStoreName))
+            throw new SecretException("Secret Store Name couldn't be null or empty string. Provide a valid Secret Store Name");
+        }
+        try
+        {
+            var daprClient = new DaprClientBuilder().Build();
+            foreach (var secretPath in secretPaths)
             {
-                throw new SecretException("Secret Store Name couldn't be null or empty string. Provide a valid Secret Store Name");
-            }
-            try
-            {
-                var daprClient = new DaprClientBuilder().Build();
-                foreach (var secretPath in secretPaths)
-                {
-                    var secret = await daprClient.GetSecretAsync(secretStoreName, secretPath);
+                var secret = await daprClient.GetSecretAsync(secretStoreName, secretPath);
 
-                    builder.AddInMemoryCollection(secret);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new SecretException("An Error Occured At Dapr Secret Store. Detail:" + ex);
+                builder.AddInMemoryCollection(secret);
             }
         }
-
-        public class SecretException : Exception
+        catch (Exception ex)
         {
-            public SecretException(string message) : base(message) { }
+            throw new SecretException("An Error Occured At Dapr Secret Store. Detail:" + ex);
         }
+    }
+    public static async Task AddVaultSecrets(this IConfigurationBuilder builder, string? secretStoreName, string[] secretPaths, DaprClient daprClient)
+    {
+        if (string.IsNullOrWhiteSpace(secretStoreName))
+        {
+            throw new SecretException("Secret Store Name couldn't be null or empty string. Provide a valid Secret Store Name");
+        }
+        await builder.AddSecretsAsync(secretStoreName, secretPaths, daprClient);
+    }
+    private static async Task AddSecretsAsync(this IConfigurationBuilder builder, string? secretStoreName, string[] secretPaths, DaprClient daprClient)
+    {
+        try
+        {
+            foreach (var secretPath in secretPaths)
+            {
+                var secret = await daprClient.GetSecretAsync(secretStoreName, secretPath);
+
+                builder.AddInMemoryCollection(secret);
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new SecretException("An Error Occured At Dapr Secret Store. Detail:" + ex);
+        }
+    }
+    public class SecretException : Exception
+    {
+        public SecretException(string message) : base(message) { }
     }
 }
 
