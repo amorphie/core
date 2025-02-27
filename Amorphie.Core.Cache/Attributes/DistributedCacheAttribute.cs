beba21eb198a4f0b387964d18b242cb7c3fb688e
Tayfun Yılmaz <tayfun.yilmaz2@hotmail.com>
2025-02-27 12:54:19 +0300

    Cache format added in DistributedCache

--- Amorphie.Core.Cache/Attributes/DistributedCacheAttribute.cs
+++ Amorphie.Core.Cache/Attributes/DistributedCacheAttribute.cs
@@ -5,11 +5,17 @@ public class DistributedCacheAttribute : Attribute
     public int TimeToLiveMinutes { get; set; }
     public List<string> HeadersToDiffer { get; set; } = new List<string>();
     public string? OverridePathWith { get; set; }
+    public string? CacheFormat { get; set; }
 
-    public DistributedCacheAttribute(int timeToLiveMinutes, string[]? headersToDiffer = null, string? overridePathWith = null)
+    public DistributedCacheAttribute(
+        int timeToLiveMinutes, 
+        string[]? headersToDiffer = null, 
+        string? overridePathWith = null,
+        string? cacheFormat = null)
     {
         TimeToLiveMinutes = timeToLiveMinutes;
         OverridePathWith = overridePathWith;
+        CacheFormat = cacheFormat;
         HeadersToDiffer ??= headersToDiffer.ToList();
     }
 }
